8ab7dbb81679424c55fdfb306a8db12d499a59e9
Tony Bowden <tony@tmtm.com>
2022-04-06 11:30:36 +0300

    make comparisons less verbose

--- sources/cabinet-official/compare.sh
+++ sources/cabinet-official/compare.sh
@@ -2,8 +2,8 @@
 
 cd $(dirname $0)
 
-bundle exec ruby scraper.rb | ifne tee scraped.csv
-bundle exec ruby wikidata.rb meta.json | sed -e 's/T00:00:00Z//g' | qsv dedup -s psid | ifne tee wikidata.csv
+bundle exec ruby scraper.rb > scraped.csv
+bundle exec ruby wikidata.rb meta.json | sed -e 's/T00:00:00Z//g' | qsv dedup -s psid > wikidata.csv
 bundle exec ruby diff.rb | tee diff.csv
 
 cd -
--- sources/senate-current/compare.sh
+++ sources/senate-current/compare.sh
@@ -2,8 +2,8 @@
 
 cd $(dirname $0)
 
-jq -r '.data[] | @csv' official.json | sed -e 's/  / /g' | qsv rename -n name,state,constituency,party,id | qsv sort -N -s id | ifne tee scraped.csv
-wd sparql -f csv wikidata.js | sed -e 's/T00:00:00Z//g' -e 's#http://www.wikidata.org/entity/##g' | qsv dedup -s psid | ifne tee wikidata.csv
+jq -r '.data[] | @csv' official.json | sed -e 's/  / /g' | qsv rename -n name,state,constituency,party,id | qsv sort -N -s id > scraped.csv
+wd sparql -f csv wikidata.js | sed -e 's/T00:00:00Z//g' -e 's#http://www.wikidata.org/entity/##g' | qsv dedup -s psid > wikidata.csv
 bundle exec ruby diff.rb | qsv sort -s name | tee diff.csv
 
 cd -
