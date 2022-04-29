47c9e62662b377e2a7ab9b74b62aa25ad03a3841
Tony Bowden <tony@tmtm.com>
2022-04-29 10:54:23 +0300

    Include date+lable in references

--- sources/senate-8/add-p39.js
+++ sources/senate-8/add-p39.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 let rawmeta = fs.readFileSync('meta.json');
 let meta = JSON.parse(rawmeta);
 
-module.exports = (id, district) => {
+module.exports = (id, label, district) => {
   position = 'Q19822359'
 
   qualifier = { P2937: meta.term.id }
@@ -14,7 +14,11 @@ module.exports = (id, district) => {
       P39: {
         value: position,
         qualifiers: qualifier,
-        references: { P4656: meta.source }
+        references: {
+          P4656: meta.source,
+          P813: new Date().toISOString().split('T')[0],
+          P1810: label,
+        }
       }
     }
   }
