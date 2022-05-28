162524ac61dd54db976a72339bbfff7b6fb9c33e
Tony Bowden <tony@tmtm.com>
2022-05-28 12:37:20 +0300

    update to latest wpoffice

--- sources/security-director/scraper.rb
+++ sources/security-director/scraper.rb
@@ -18,16 +18,8 @@ class OfficeholderList < OfficeholderListBase
       %w[name dates].freeze
     end
 
-    def item_link
+    def name_node
       name_cell.css('a').last
-    end
-
-    def item
-      item_link.attr('wikidata')
-    end
-
-    def itemLabel
-      item_link.text
     end
   end
 end
